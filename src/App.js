4f74ae6b43bbbc723e8e4ef4fa1e74b3143b36b9
sanjay-tringapps <96760013+sanjay-tringapps@users.noreply.github.com>
2022-05-19 13:54:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Info from './Components/Info';
+function App() {
+  return (
+    <div className ="card" >
+     <Info/>
+     <About/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
